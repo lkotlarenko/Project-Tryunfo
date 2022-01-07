8af821e6e37c5ddafabc739aed52394a38cf608f
lkotlarenko <lkotlarenko@protonmail.com>
2022-01-07 18:09:35 -0300

    Creating "Form" & "InputHere"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Form from './components/Form';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <main>
         <h1>Tryunfo</h1>
-        <h2>Hello world!</h2>
-      </div>
+        <Form />
+      </main>
     );
   }
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <section>
+        <h3>Hello world</h3>
+      </section>
+    );
+  }
+}
+
+export default Form;
