3974491b77d3f265cd49a02afc43dbdd476a898c
lkotlarenko <lkotlarenko@protonmail.com>
2022-01-14 19:49:16 -0300

    adding comments :smile:

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ const cleanState = {
   cardTrunfo: false,
   isSaveButtonDisabled: true,
 };
+// clean state to use when saving a new card
+
 const initialState = {
   cardName: '',
   cardDescription: '',
@@ -41,25 +43,36 @@ class App extends React.Component {
 
   onSaveButtonClick = () => {
     const newCard = { ...this.state };
+    // get values of new card using state
     delete newCard.deck;
+    // delete old array from copy
     this.validateTrunfo();
+    // set hasTrunfo true if newCard is trunfo
     this.setState(cleanState);
-    this.setState(({ deck }) => ({ deck: [...deck, newCard] }), console.log(this.deck));
+    // reset values from all inputs (cleaning only the right state values)
+    this.setState(({ deck }) => ({ deck: [...deck, newCard] }));
+    // set state 'deck' array adding the new card to end of the array
   }
 
   onInputChange = ({ target }) => {
+    // check if input is a checkbox and return the right value
     const rightValue = target.type === 'checkbox'
       ? target.checked
       : target.value;
     this.setState({ [target.name]: rightValue }, this.validateInputs);
-    // validate on every change
+    // link state values to inputs accordingly &&
+    // validate inputs on every change
   }
 
   deleteCard = (cardToRemove) => {
+    // receive the card to remove
     const { deck } = this.state;
+    // get deck array from state
     if (cardToRemove.cardTrunfo) { this.setState({ hasTrunfo: false }); }
+    // check if the card being removed is a trunfo, if so set hasTrunfo to false
     const cardRemoved = deck.filter((card) => card.cardName !== cardToRemove.cardName
     && card.cardDescription !== cardToRemove.cardDescription);
+    // generate a new deck array filtering the remove card by matching name and description
     this.setState({ deck: [...cardRemoved] });
   }
 
@@ -89,27 +102,32 @@ class App extends React.Component {
       && Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= sum
     ) return this.setState({ isSaveButtonDisabled: false });
     this.setState({ isSaveButtonDisabled: true });
-  }
+  } // needs refactoring :P
+  // set isSaveButtonDisabled according to valid input values
 
   render() {
     const { state, state: { deck }, onInputChange, onSaveButtonClick } = this;
+    // destructuring state (to pass as props), deck array from state (to use on preview) & functions used
     return (
       <>
         <main>
           <h1>Tryunfo</h1>
           <Form
             { ...state }
+            // passing props from state using spread
             onSaveButtonClick={ onSaveButtonClick }
             onInputChange={ onInputChange }
           />
           <Card
             { ...state }
             cardPreview
+            // prop to change CSS class
           />
         </main>
         {
           deck.map((card) => {
             const id = Math.round(Math.random() * 100);
+            // generate a id to use as key
             return (
               <section
                 key={ `${card.cardName}${id}` }
@@ -120,6 +138,7 @@ class App extends React.Component {
                   type="button"
                   data-testid="delete-button"
                   onClick={ () => this.deleteCard(card) }
+                  // onClick pass target to deleteCard
                 >
                   Delete Card
                 </button>
